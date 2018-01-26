d84b48a96d76fd4289a16054cb7ea3b8a2383a06
TripleZ <me@triplez.cn>
2018-01-26 18:50:02 +0800

    Finish 5.3 series tasks

---
+++ C5/5.3.py
@@ -0,0 +1,76 @@
+# 5-3
+# alien_color = 'red'
+# if alien_color == 'green':
+#     print("You get 5 points!")
+#
+# alien_color = 'green'
+# if alien_color == 'green':
+#     print("You get 5 points!")
+
+# 5-4
+# alien_color = 'red'
+# if alien_color == "green":
+#     print("You get 5 points!")
+# else:
+#     print("You get 10 points!")
+#
+# alien_color = 'green'
+# if alien_color == "green":
+#     print("You get 5 points!")
+# else:
+#     print("You get 10 points!")
+
+# 5-5
+# alien_color = 'yellow'
+# if alien_color == 'green':
+#     print("You get 5 points!")
+# elif alien_color == 'yellow':
+#     print("You get 10 points!")
+# elif alien_color == 'red':
+#     print("You get 15 points!")
+#
+# alien_color = 'red'
+# if alien_color == 'green':
+#     print("You get 5 points!")
+# elif alien_color == 'yellow':
+#     print("You get 10 points!")
+# elif alien_color == 'red':
+#     print("You get 15 points!")
+#
+# alien_color = 'green'
+# if alien_color == 'green':
+#     print("You get 5 points!")
+# elif alien_color == 'yellow':
+#     print("You get 10 points!")
+# elif alien_color == 'red':
+#     print("You get 15 points!")
+
+# 5-6
+# age = 20
+#
+# if age >= 0 and age < 2:
+#     print('Baby')
+# elif age >= 2 and age < 4:
+#     print('Learning walk')
+# elif age >= 4 and age < 13:
+#     print("Child")
+# elif age >= 13 and age < 20:
+#     print("Teenager")
+# elif age >= 20 and age < 65:
+#     print('Adult')
+# elif age >=65:
+#     print('Old')
+
+# 5-7
+favorite_fruits = ['apple', 'mango', 'pear']
+for fruit in favorite_fruits:
+    if fruit == 'banana':
+        print('You really like bananas')
+    elif fruit == 'apple':
+        print('You really like apples')
+    elif fruit == 'peach':
+        print('You really like peaches')
+    elif fruit == 'orange':
+        print('You really like oranges')
+    elif fruit == 'mango':
+        print('You really like mangoes')
