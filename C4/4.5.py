4c94708046711fa5167a929b1d9345ee6d1fba8a
TripleZ <me@triplez.cn>
2018-01-26 18:00:16 +0800

    Finish 4.5 task

---
+++ C4/4.5.py
@@ -0,0 +1,13 @@
+# 4-13
+foods = ('meat', 'steak', 'chips', 'burger', 'coke')
+
+for food in foods:
+    print(food)
+
+# Test tuple is unchangeable
+# foods[0] = 'test'
+
+foods = ('meat', 'vegetable', 'chicken', 'burger', 'coke')
+print()
+for food in foods:
+    print(food)
