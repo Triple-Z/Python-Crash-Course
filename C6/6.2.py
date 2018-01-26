469b837d1409635f94409d29051dce5c75e0c7b7
TripleZ <me@triplez.cn>
2018-01-26 22:14:46 +0800

    Finish 6.2 series tasks

---
+++ C6/6.2.py
@@ -0,0 +1,33 @@
+# 6-1
+uncleblue = {}
+uncleblue['first_name'] = 'Jiang'
+uncleblue['last_name'] = 'Wanshu'
+uncleblue['age'] = 20
+uncleblue['city'] = 'Tianjin'
+
+print(uncleblue)
+
+# 6-2
+favorite_numbers = {}
+favorite_numbers['triple z'] = 7
+favorite_numbers['uncleblue'] = 8
+favorite_numbers['test1'] = 1
+favorite_numbers['test2'] = 2
+favorite_numbers['test3'] = 3
+
+print(favorite_numbers)
+
+# 6-3
+dictionary = {
+    'a': 'A',
+    'b': 'B',
+    'c': 'C',
+    'd': 'D',
+    'e': 'E',
+}
+print(dictionary['a'])
+print(dictionary['b'])
+print(dictionary['c'])
+print(dictionary['d'])
+print(dictionary['e'])
+
