5348c84fbbcb091dbc738499b11d61476bc5c691
TripleZ <me@triplez.cn>
2018-02-03 22:41:16 +0800

    :tada: Finish 18.4 series tasks

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -6,5 +6,9 @@ app_name = 'learning_logs'
 
 urlpatterns = [
     # Home page
-    re_path(r'^$', views.index, name='index')
+    re_path(r'^$', views.index, name='index'),
+    # Show all the topics
+    re_path(r'^topics/$', views.topics, name='topics'),
+    # Specific topic
+    re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
 ]
