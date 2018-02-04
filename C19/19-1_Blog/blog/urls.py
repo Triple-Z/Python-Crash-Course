fa0ae8d40a5b360703b2ef73553db5c31dbe3f03
TripleZ <me@triplez.cn>
2018-02-04 22:13:00 +0800

    :tada: Finish 19.2 task

--- C19/19-1_Blog/blog/urls.py
+++ C19/19-1_Blog/blog/urls.py
@@ -19,4 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogs.urls', namespace='blogs')),
+    path('users/', include('users.urls', namespace='users')),
+
 ]
+
--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # My application
+    # My applications
     'learning_logs',
+    'users',
 ]
 
 MIDDLEWARE = [
